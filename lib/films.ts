80ae80703f19e0a38fc4ecf17a3738ed12d70456
guerrafilmes <guerra@andreguerrafilmes.com.br>
2025-08-13 12:02:07 -0300

    Add files via upload

--- lib/films.ts
+++ lib/films.ts
@@ -1,6 +1,5 @@
-// IDs extraídos dos links enviados
 export const films = [
-  { id: 'btvrVukw68k', title: 'Filme 1 — Destaque' },
-  { id: 'QB-96u7Ty3c', title: 'Filme 2' },
-  { id: 'hq8IJJCcm7k', title: 'Filme 3' },
+  { id: 'btvrVukw68k', title: 'Tayenne & João — Teaser' },
+  { id: 'QB-96u7Ty3c', title: 'Casamento — Destaque' },
+  { id: 'hq8IJJCcm7k', title: 'Casamento — Destaque' },
 ]
